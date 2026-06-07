c075a88794065b657de3c77d07440b3941598157
Arpit Maurya <arpitmaurya@Arpits-MacBook-Air.local>
2026-06-07 11:33:47 +0530

    Added JWT authentication middleware and protected route

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.header("Authorization");
+
+  if (!authHeader) {
+    return res.status(401).json({
+      message: "Access Denied"
+    });
+  }
+
+  const token = authHeader.startsWith("Bearer ")
+    ? authHeader.split(" ")[1]
+    : authHeader;
+
+  try {
+    const verified = jwt.verify(
+      token,
+      process.env.JWT_SECRET
+    );
+
+    req.user = verified;
+    next();
+  } catch (err) {
+    console.log(err.message);
+
+    return res.status(400).json({
+      message: "Invalid Token"
+    });
+  }
+};
+
+module.exports = authMiddleware;
--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,19 @@ require("dotenv").config();
 
 const db = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Auth Routes
+// Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
+// Home Route
 app.get("/", (req, res) => {
   res.send("NewsPulse Backend Running 🚀");
 });
